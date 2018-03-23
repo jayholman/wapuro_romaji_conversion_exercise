cb6e95705a0662af7f64117d988851c807483a1f
Jay Holman <jay@wistia.com>
2018-03-23 17:55:42 -0400

    Add geminates logic

--- wapuro_romaji_conversion_v2.rb
+++ wapuro_romaji_conversion_v2.rb
@@ -1,5 +1,6 @@
 def convert_string(string)
   conversion_chart = { "n"=>"ん","a"=>"あ","i"=>"い","u"=>"う","e"=>"え","o"=>"お", "ka"=>"か","ki"=>"き","ku"=>"く","ke"=>"け","ko"=>"こ","sa"=>"さ","su"=>"す","se"=>"せ","so"=>"そ","ta"=>"た","te"=>"て","to"=>"と","na"=>"な","ni"=>"に","nu"=>"ぬ","ne"=>"ね","no"=>"の","ha"=>"は","hi"=>"ひ","fu"=>"ふ","he"=>"へ","ho"=>"ほ","ma"=>"ま","mi"=>"み","mu"=>"む","me"=>"め","mo"=>"も","ya"=>"や","yu"=>"ゆ","yo"=>"よ","ra"=>"ら","ri"=>"り","ru"=>"る","re"=>"れ","ro"=>"ろ","wa"=>"わ","wi"=>"ゐ","we"=>"ゑ","wo"=>"を","ga"=>"が","gi"=>"ぎ","gu"=>"ぐ","ge"=>"げ","go"=>"ご","za"=>"ざ","ji"=>"じ","zu"=>"ず","ze"=>"ぜ","zo"=>"ぞ","da"=>"だ","di"=>"ぢ","du"=>"づ","de"=>"で","do"=>"ど","ba"=>"ば","bi"=>"び","bu"=>"ぶ","be"=>"べ","bo"=>"ぼ","pa"=>"ぱ","pi"=>"ぴ","pu"=>"ぷ","pe"=>"ぺ","po"=>"ぽ", "shi"=>"し","chi"=>"ち","tsu"=>"つ", " "=>" "}
+  geminates = ["kk", "ss", "tt", "pp", "cc"]
   current_character = 0
   converted_string = ""
   broken_character_array = string.scan(/.{1}/)
@@ -7,7 +8,11 @@ def convert_string(string)
     character = broken_character_array[current_character]
     double_character = broken_character_array[current_character + 1] ? character + broken_character_array[current_character + 1] : nil
     triple_character = broken_character_array[current_character + 2] ? double_character + broken_character_array[current_character +2] : nil
-    if conversion_chart.key?(double_character)
+
+    if geminates.include?(double_character) && broken_character_array[current_character - 1] != nil && broken_character_array[current_character - 1] != " "
+      converted_string += "っ"
+      current_character += 1
+    elsif conversion_chart.key?(double_character)
       converted_string += conversion_chart[double_character]
       current_character += 2
     elsif conversion_chart.key?(character)
